0446a00f520b3df223a8615e996561b7a46681c3
JuanJoven01 <juanjov95@hotmail.com>
2024-03-16 15:26:52 -0500

    created home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,34 @@
+import { Title } from "../../components/Title"
+
 const Home = () => {
 
     return(
         <div>
-            <h1>Home</h1>
+            <Title text='Bienvenido a la puesta en vivo del reto de programación!' />
+            <p className="mt-5 font-semibold">En la pestaña de empleados puedes: </p>
+            <ul className="mt-5" >
+                <li>
+                    Crear un empleado
+                </li>
+                <li>
+                    Ver todos los empelados
+                </li>
+                <li>
+                    Filtrar empleados por el nombre
+                </li>
+            </ul>
+            <p className="mt-5 font-semibold">En la pestaña de solicitudes puedes: </p>
+            <ul className="mt-5" >
+                <li>
+                    Crear una solicitud
+                </li>
+                <li>
+                    Ver todas las solicitudes
+                </li>
+                <li>
+                    Eliminar una solicitud previamente identificada
+                </li>
+            </ul>
         </div>
     )
 }
